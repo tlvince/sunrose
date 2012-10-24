96e1fa036b4b0537e1034c8fa1401e975a196d78
Tom Vincent <git@tlvince.com>
2012-10-24 18:45:46 +0800

    Add usage, source in subshell

--- sunrose.sh
+++ sunrose.sh
@@ -4,9 +4,16 @@
 
 modules="${modules:-sunrose.d}"
 
+usage() { echo "[module=/path/to/sunrose.d] $0 light||dark"; }
+
+[ "$1" ] || { usage && exit 1; }
+
 # $1 - String: 'light' or 'dark'
-[ -d "$modules" ] && {
+if [ -d "$modules" ]; then
   for module in $modules/*.sh; do
-    [ -r "$module" ] && . "$module" && "$1"
+    if [ -r "$module" ] && grep --quiet "$1()" "$module"; then
+      # Source in a sub shell to clear previous module
+      "${SHELL:-/bin/sh}" -c ". $module && $1"
+    fi
   done
-}
+fi
