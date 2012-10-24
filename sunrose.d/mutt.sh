72c8c8ba8c800469bc2cccbf319c7edc04cebc6e
Tom Vincent <git@tlvince.com>
2012-10-24 18:45:57 +0800

    Add mutt module

---
+++ sunrose.d/mutt.sh
@@ -0,0 +1,13 @@
+#!/bin/dash
+
+light="${light:-${XDG_CONFIG:-$HOME/.config}/mutt/colour/solarized/mutt-colors-solarized-light-16.muttrc}"
+dark="${dark:-${XDG_CONFIG:-$HOME/.config}/mutt/colour/solarized/mutt-colors-solarized-dark-16.muttrc}"
+
+muttrc="${muttrc:-${XDG_CONFIG:-$HOME/.config}/mutt/muttrc}"
+
+switch() {
+  sed -i "$1" "$muttrc"
+}
+
+light() { switch "s|$dark|$light|"; }
+dark()  { switch "s|$light|$dark|"; }
