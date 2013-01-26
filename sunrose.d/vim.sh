d8b9f792299c313bee6927e844e293e1c0bc4371
Tom Vincent <git@tlvince.com>
2013-01-26 21:08:08 +0800

    DRY vim module

--- sunrose.d/vim.sh
+++ sunrose.d/vim.sh
@@ -3,6 +3,6 @@
 config="${XDG_CONFIG_HOME:-$HOME/.config}/sunrose/vim"
 [ -f "$config" ] && source "$config" || exit 1
 
-switch() { sed -i "$1" "$vimrc"; }
-light() { switch "s|^set background=dark$|set background=light|"; }
-dark() { switch "s|^set background=light$|set background=dark|"; }
+switch() { sed -i "s|^set background=$1$|set background=$2|" "$vimrc"; }
+light() { switch dark light; }
+dark() { switch light dark; }
